21540cf12647bd4ad6ac67442045b7103b931cc4
kaustubhshukla9586 <kaustubhshukla9586@gmail.com>
2026-03-27 00:24:56 +0530

    publickey

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const EMAILJS_SERVICE_ID = 'service_az4400a';
 const EMAILJS_TEMPLATE_ID = 'template_ove2j9k';
-const EMAILJS_PUBLIC_KEY = 'WSr2Zox6TnvX7osv';
+const EMAILJS_PUBLIC_KEY = '-WSr2Zox6TnvX7osv';
